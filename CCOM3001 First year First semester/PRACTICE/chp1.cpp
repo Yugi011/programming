4e135136432d7744ab464819a602daf9c8d84f8d
Yugi011 <adriangaby981@gmail.com>
2025-09-23 11:34:39 -0400

    new program made for practice.

---
+++ CCOM3001 First year First semester/PRACTICE/chp1.cpp
@@ -0,0 +1,30 @@
+/*
+Algoritmo:
+1.declarar variables
+2.entrar resultados del examen
+3.calcular ponderacion (formula = (a + b + c + d) / 400 * 100)
+4.imprimir resultado de ponderacion
+*/
+#include <iomanip>
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    double testScore1, testScore2, testScore3, testScore4, weightTestScore;
+    cout << "ENTER SCORE FOR EXAM 1:";
+    cin >> testScore1;
+    cout << "ENTER SCORE FOR EXAM 2:";
+    cin >> testScore2;
+    cout << "ENTER SCORE FOR EXAM 3:";
+    cin >> testScore3;
+    cout << "ENTER SCORE FOR EXAM 4:";
+    cin >> testScore4;
+    weightTestScore = (testScore1 + testScore2 + testScore3 + testScore4) / 400;
+    cout << fixed << setprecision(2);
+    cout << endl;
+    cout << "-----------------------------------------------------------" << endl;
+    cout << setw(30) << right << "Weighted Test Score: " << weightTestScore * 100 << "%" << endl;
+    cout << "-----------------------------------------------------------" << endl;
+    return 0;
+}
